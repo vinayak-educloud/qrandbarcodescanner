33a7de3e50fb0e2d0d8f52bbd905e2642fe30fd7
vinayak-educloud <vinayak.educloud@gmail.com>
2025-04-02 19:12:54 +0530

    init

---
+++ example/android/app/src/main/java/com/amolg/flutterbarcodescannerexample/MainActivity.java
@@ -0,0 +1,8 @@
+package com.vinayak.scannerexample;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+  // You can keep this empty class or remove it. Plugins on the new embedding
+  // now automatically registers plugins.
+}
